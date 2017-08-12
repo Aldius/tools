11d93bc1a84fa19f4bcd2afdbb42ee0d2cec78e3
Aldius <aldius@inf.elte.hu>
2017-08-12 18:32:45 +0200

    Remote interfaces specified.

--- gomoku/src/main/java/hu/elte/tools/assignment/backend/GomokuServerIface.java
+++ gomoku/src/main/java/hu/elte/tools/assignment/backend/GomokuServerIface.java
@@ -1,12 +1,21 @@
 package hu.elte.tools.assignment.backend;
 
+import hu.elte.tools.assignment.shared.model.GomokuModel;
+
 import java.rmi.Remote;
+import java.rmi.RemoteException;
 
 /**
  * Created by cmwal on 2017. 08. 11..
  */
 public interface GomokuServerIface extends Remote{
 
+	boolean login(String userName, String password) throws RemoteException;
 
+	boolean register(String userName,  String password) throws RemoteException;
+
+	void searchForMatch(String userName, String address) throws RemoteException;
+
+	void synchronizeModel(GomokuModel model) throws RemoteException;
 
 }
--- gomoku/src/main/java/hu/elte/tools/assignment/frontend/GomokuClientIface.java
+++ gomoku/src/main/java/hu/elte/tools/assignment/frontend/GomokuClientIface.java
@@ -1,12 +1,21 @@
 package hu.elte.tools.assignment.frontend;
 
+import hu.elte.tools.assignment.shared.model.GomokuModel;
+
 import java.rmi.Remote;
+import java.rmi.RemoteException;
 
 /**
  * Created by cmwal on 2017. 08. 11..
  */
 public interface GomokuClientIface extends Remote {
 
-	
+	void startGame() throws RemoteException;
+
+	void yourTurn() throws RemoteException;
+
+	void synchronizeModel(GomokuModel model) throws RemoteException;
+
+	void endGame(boolean winner) throws RemoteException;
 
 }
